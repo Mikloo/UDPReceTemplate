38f98a1b8571b873402a4e9d7bf81d46b1d73c48
Mikail Fener <mika1292@edu.easj.dk>
2019-06-11 15:28:32 +0200

    Add project files.

---
+++ UDPReceTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDPReceTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
